90e900af87c034a9720bcbc40f2b0a68a2fb99f6
Herosbrine <gregoire.bezier@epitech.eu>
2020-01-28 02:25:51 +0100

    [ADD] | unsetenv

--- src/my_unsetenv.c
+++ src/my_unsetenv.c
@@ -12,7 +12,27 @@
 #include "printf/my.h"
 #include <stdio.h>
 
-int my_unsetenv(char **argv, char *path)
+int my_unsetenv2(char **argv, char **envp)
+{
+    int i = 1;
+    int a = 0;
+    char *toto = NULL;
+
+    while (argv[i] != NULL) {
+        toto = my_strcat2(argv[i], "=");
+        while (envp[a] != NULL) {
+            if (my_strcmp(envp[a], toto, my_strlen(toto) - 1 ) == 0) {
+                envp[a] = NULL;
+            }
+            a++;
+        }
+        a = 0;
+        i++;
+    }
+    return (0);
+}
+
+int my_unsetenv(char **argv, char **envp)
 {
     int i = 0;
     char *dest = NULL;
@@ -20,10 +40,13 @@ int my_unsetenv(char **argv, char *path)
     dest = "unsetenv";
     while (argv[i] != NULL) {
         if (my_strcmp(argv[i], dest, 8) == 0) {
-            if (path == NULL) {
+            if (argv[1] == NULL) {
                 my_printf("%s: Too few arguments.\n", dest);
                 return (0);
             }
+            else
+                my_unsetenv2(argv, envp);
+            return (0);
         i++;
         }
         return (1);
--- src/printf/my.h
+++ src/printf/my.h
@@ -76,7 +76,7 @@ char **my_str_envp_array(int *a, char *buffer);
 char *my_strcat(char *dest, char *src);
 char *my_strdup(char *str);
 int my_cd(char **argv, char *path, char *envp, char *envi);
-int my_unsetenv(char **argv, char *path);
+int my_unsetenv(char **argv, char **envp);
 int find_home_path(char **src);
 int my_setenv(char **argv, char *path, char *path2, char **envp);
 char *my_strcat2(char *dest, char *src);
