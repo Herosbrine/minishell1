655629f5ec138ca91311d8c7d14bd2db5a151483
Herosbrine <gregoire.bezier@epitech.eu>
2020-01-31 19:28:42 +0100

    [ADD] | unsetenv and setenv return 0

--- src/my_str_envp_array.c
+++ src/my_str_envp_array.c
@@ -19,13 +19,13 @@ char **my_str_envp_array(int *a, char *buffer)
     a_position = malloc(sizeof(char * ) * *a + 1);
     while (r < *a) {
         temp = malloc(sizeof(char) * my_strlen(buffer));
-        i = 0;
         while (buffer[i] != ':' && buffer[i] != '\0') {
             temp[i] = buffer[i];
             i++;
         }
         buffer = &buffer[i+1];
         a_position[k] = temp;
+        i = 0;
         k++;
         r++;
     }
